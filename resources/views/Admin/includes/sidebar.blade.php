1d65a8392dfce853ba6f3029817791181353a1c1
Oleh_WoWDeveloper <oleh10836@gmail.com>
2022-09-25 13:06:46 +0300

    ass page with category

---
+++ resources/views/Admin/includes/sidebar.blade.php
@@ -0,0 +1,18 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <ul class="pt-3 nav nav-pills nav-sidebar flex-column">
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-th-list"></i>
+                    <p>
+                        Category
+                        <span class="badge badge-info right">2</span>
+                    </p>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!-- /.sidebar -->
+</aside>
