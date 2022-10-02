0e0a97cdb3f4026ee7b3067d4d1119776ae9261b
Oleh_WoWDeveloper <oleh10836@gmail.com>
2022-10-02 22:35:39 +0300

    add opportunity change category

--- resources/views/Admin/categories/show.blade.php
+++ resources/views/Admin/categories/show.blade.php
@@ -6,8 +6,11 @@
         <div class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">Dashboard</h1>
+                    <div class="col-sm-6 d-flex align-items-center">
+                        <h1 class="m-0 mr-2">{{$category->title}}</h1>
+                        <a class="text-success" href={{route('admin.category.edit', $category->id)}}>
+                            <i class="fas fa-pencil-alt"></i>
+                        </a>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
@@ -34,31 +37,15 @@
                 </div>
                 <div class="row card-body table-responsive">
                     <table class="table table-hover text-nowrap col-6">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Action</th>
-                                <th>Edit</th>
-                            </tr>
-                        </thead>
                         <tbody>
-                        @foreach($categories as $category)
                         <tr>
-                            <td>{{$category->id}}</td>
+                            <td>ID</td>
                             <td>{{$category->title}}</td>
-                            <td>
-                                <a href={{route('admin.category.show', $category->id)}}>
-                                    <i class="far fa-eye"></i>
-                                </a>
-                            </td>
-                            <td>
-                                <a href={{route('admin.category.edit', $category->id)}}>
-                                    <i class="fas fa-pencil-alt"></i>
-                                </a>
-                            </td>
                         </tr>
-                        @endforeach
+                        <tr>
+                            <td>Title</td>
+                            <td>{{$category->title}}</td>
+                        </tr>
                         </tbody>
                     </table>
                 </div>
